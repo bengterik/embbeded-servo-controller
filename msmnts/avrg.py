dabc643a530b510f61bc3b60bbcd2b869a312592
bengterik <52504991+bengterik@users.noreply.github.com>
2022-12-13 16:22:40 +0100

    Measurements on ticks

--- msmnts/avrg.py
+++ msmnts/avrg.py
@@ -8,7 +8,8 @@ def main():
     infile = open(filename, 'r')
     numbers = []
     for line in infile:
-        numbers.append(float(line))
+        if (not line.startswith('\n')):
+            numbers.append(float(line))
 
     total = 0.0
     for num in numbers:
@@ -23,5 +24,8 @@ def main():
     # print the results
     print('Average: %.2f' % average)
     print('Standard deviation: %.2f' % std_dev)
+    print('Procent of values above 15500 = %.5f' % (len([i for i in numbers if i > 15500])))
+    print('Procent of values below 650 = %.5f' % len([i for i in numbers if i < 650]))
+    print('Length is %d' % len(numbers))
 
 main()

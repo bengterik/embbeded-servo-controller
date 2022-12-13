70c9c2dd279e2b44dc3b7220a08756953e525309
bengterik <52504991+bengterik@users.noreply.github.com>
2022-12-13 16:04:22 +0100

    Measure on avr output

---
+++ msmnts/112.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+
+# read in numbers from a file and get their average and standard deviation
+def main():
+    filename = sys.argv[1]
+    infile = open(filename, 'r')
+    numbers = []
+    for line in infile:
+        numbers.append(float(line))
+
+    total = 0.0
+    for num in numbers:
+        total += num
+    average = total / len(numbers)
+
+    total = 0.0
+    for num in numbers:
+        total += (num - average)**2
+    std_dev = math.sqrt(total / len(numbers))
+
+    # print the results
+    print('Average: %.2f' % average)
+    print('Standard deviation: %.2f' % std_dev)
+
+main()

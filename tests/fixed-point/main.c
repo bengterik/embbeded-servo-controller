f83ffd497a9f7f81d0f4357d635e6ca0905548fe
bengterik <52504991+bengterik@users.noreply.github.com>
2022-12-15 15:01:31 +0100

    Fixed-point mul and div for unsigned

--- tests/fixed-point/main.c
+++ tests/fixed-point/main.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
+#include <stdint.h>
 
-void modulo() {
-    int a = 10;
+#define SHIFT_AMOUNT 8 
 
-    for (int b = -10; b < a; b++) {     
-        int result = a % b; 
-        printf("%d mod %d = %d\n", b, a, b % a);
-    }
+typedef uint16_t fp_int;
+
+uint8_t fp_mul(uint8_t a, fp_int b) {
+    fp_int temp = (fp_int) a * b;
+
+    uint8_t res = temp >> SHIFT_AMOUNT;
+
+    return res;
+}
+
+uint8_t fp_div(uint8_t a, fp_int b) {
+    fp_int temp = (uint16_t) a << SHIFT_AMOUNT;
+    temp += b >> 1;
+    printf("a: %d, b: %d, temp: %d\n", a, b, temp);
+    uint16_t res = (temp / b);
+    printf("res: %d\n", res);
+
+    return res;
 }
 
 int main() {
-    modulo();
-    return 0;
+    printf("\n");
+    uint8_t a = 120;
+    fp_int b = 0x0080; // 0.5 in fixed point
+    uint16_t c;
+    
+    c = fp_mul(a, b);
+    printf("%d * 0.5 = %d\n", a, c);
+
+    printf("\n");
+    b = 0x200; // 2 in fixed point
+    c = fp_div(a, b);
+    printf("%d / 2 = %d\n", a, c);
+    printf("\n");
+
+    printf("\n");
+    b = 0x300; // 2 in fixed point
+    c = fp_div(a, b);
+    printf("%d / 3 = %d\n", a, c);
+    printf("\n");
 }
+
